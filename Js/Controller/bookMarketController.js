65d733087ac350372b0855f1dbd46acf635f7ebc
CallMeTommy <tlstommy@qq.com>
2015-10-01 22:10:59 +0800

    uiRouter Note. Next day go on

--- Js/Controller/bookMarketController.js
+++ Js/Controller/bookMarketController.js
@@ -8,3 +8,28 @@ bookControllers.controller('homePage',function($scope){
     $scope.title = 'welcome to BookMarket';
 });
 
+bookControllers.controller('templateCtrl',function($scope){
+    $scope.title = ' this is template display';
+});
+
+bookControllers.controller('stringAsCtrl',function($scope){
+    $scope.title = ' this is stringAs display';
+
+    this.stringAs = "wolegequ";
+});
+
+bookControllers.controller('controllerProviderNoTask',function($scope){
+    $scope.title = ' this is controllerProvider NoTask display';
+});
+
+bookControllers.controller('controllerProviderhomeWork',function($scope){
+    $scope.title = ' this is controllerProviderDoHomeWork display';
+});
+
+bookControllers.controller('controllerProvidercleanHouse',function($scope){
+    $scope.title = ' this is controllerProvider cleanHouse display';
+});
+
+bookControllers.controller('controllerProvidersleep',function($scope){
+    $scope.title = ' this is controllerProvider sleep display';
+});
--- Js/Router/BookMarket_Router.js
+++ Js/Router/BookMarket_Router.js
@@ -6,14 +6,70 @@ var routertest = angular.module("routerBook", ['ui.router']);
 
 
 routertest.config(function ($stateProvider,
-                                 $urlRouterProvider)
-{
+                            $urlRouterProvider) {
     $stateProvider.state("home", {
         url: "/",
         templateUrl: 'Tpl/uiRouter/HomePage.html',
         controller: 'homePage'
-    })
+    });
 
+
+    //templates
+    $stateProvider.state('templates', {
+        url: '/templates',
+        templateUrl: "Tpl/uiRouter/bookTemplates.html",
+        controller: 'homePage'
+    }).state('templates.template', {
+        ulr: '/template',
+        template: "<h3>{{title}}</h3>",
+        controller: 'templateCtrl'
+    }).state('templates.templateProvider', {
+        url: '/templates/templateProvider/{Id}',
+        templatProvider: function ($timeout, $stateParams) {
+            return $timeout(function () {
+                debugger;
+                var id = $stateParams.Id;
+                switch (id) {
+                    case '1':
+                        return '<h3> this is template for param1 </h3>';
+                    case '2':
+                        return '<h3> this is template for param2 </h3>';
+                    case '3':
+                        return '<h3> this is template for param3 </h3>';
+                    default :
+                        return '<h3> this is template for default </h3>';
+                }
+            });
+        }
+    });
+
+
+    //Controllers
+    $stateProvider.state("myControllers",{
+        url:'/myControllers',
+        templateUrl:'Tpl/uiRouter/bookControllers.html'
+    }).state("myControllers.typeFunction",{
+        url:"/typeFunction",
+        template:"<h3>{{title}}</h3>",
+        controller:function($scope) {
+            $scope.title = " Hi this is Controller of function ";
+        }
+    }).state('myControllers.stringAs',{
+        url:"/stringAs",
+        template:"<h3>{{title}}<small>{{ctrl.stringAs}}</small></h3>",
+        controller:"stringAsCtrl as ctrl"
+    }).state("myControllers.myProvider",{
+        url:'/myProvider/task/:taskName',
+        template:"<h3>{{title}}</h3>",
+        controllerProvider: function($stateParams) {
+            var taskName = $stateParams.taskName;
+
+            if(!taskName)
+                taskName = "NoTask";
+
+            return "controllerProvider" + taskName;
+        }
+    });
 
 
 });
